ac9c383a712e792f863ec042f3b9ec35eec34dc5
JunE <ucjune90@gmail.com>
2022-11-27 18:53:17 +0800

    Basic setup for React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./HomePage.js";
+
+function App() {
+  return (
+    <React.Fragment>
+      <HomePage />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Basic setup</h1>
+      </React.Fragment>
+    );
+  }
+}
